f001029b2b74d2a4a7f49a38838d1e1016ab4fab
Munkh-Erdene Tuul <me.tuul21@gmail.com>
2020-02-11 11:12:18 +0900

    Initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const linebot = require("linebot");
+const express = require("express");
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+const app = express();
+
+const linebotParser = bot.parser();
+
+app.post("/callback", linebotParser);
+
+bot.on("message", function(event) {
+  event
+    .reply(event.message.text)
+    .then(function(data) {
+      console.log("Success", data);
+    })
+    .catch(function(error) {
+      console.log("Error", error);
+    });
+});
+
+app.listen(5000, function() {
+  console.log("LineBot is running.");
+});
