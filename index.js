4b425bd3189ca3f8aa78fc1bf33ec06bf4c8384e
Sukhee <tab.suherdene@gmail.com>
2020-02-18 18:42:55 +0900

    merge'

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const express = require('express')
-const line = require('@line/bot-sdk');
+const express = require("express");
+const line = require("@line/bot-sdk");
 
-
-const app = express()
+const app = express();
 
 const config = {
   channelId: "1653837691",
   channelSecret: "de9040d7cbac1069321a5c80dfefc864",
   channelAccessToken:
     "Gv19n3lNQbMzkTolCguyopqUA/2ZQRT7jDBkgIUyAOXOZXe2tr76KfJxn3WIqu4DCRR9w2RkNs9sAfFXb43feMalCyqP0a2wHKN1N1Atls+kS6PJs8mOjkDUgNrgfFboX4ZSCuxxzFC7cmADJTHtLAdB04t89/1O/w1cDnyilFU="
-}
-const groupId = 'C3f4fe7ff4e6e5223810445b0146f2788';
+};
+const groupId = "C3f4fe7ff4e6e5223810445b0146f2788";
 
-const client = new line.Client(config)
+const client = new line.Client(config);
 
+client.linkRichMenuToUser(groupId, "<rich_menu_id>");
 
-app.post('/callback', line.middleware(config), (req, res) => {
-  console.log('New post')
-  req.body.events // webhook event objects
-  req.body.destination // user ID of the bot (optional)
-  console.log(req.body.events)
+app.post("/callback", line.middleware(config), (req, res) => {
+  console.log("New post");
+  req.body.events; // webhook event objects
+  req.body.destination; // user ID of the bot (optional)
+  console.log(req.body.events);
   const message1 = {
-    type: 'text',
-    text: 'Hello,'
+    type: "text",
+    text: "Hello,"
   };
-  client.multicast(['R57e49f3d31b5199d05551896db350010'],
-  [message1]
-)
-})
-
-var schedule = require('node-schedule');
- 
-var j = schedule.scheduleJob('*/30 * * * * *', function(){
- client.pushMessage(groupId, { type: 'text', text: 'Jobandwork! (' + Math.random() + ')'})
+  client.getRichMenuList().then(richmenus => {
+    console.log(richmenus)
+  });
 });
 
+var schedule = require("node-schedule");
+
+var j = schedule.scheduleJob("*/30 * * * * *", function() {
+  client.pushMessage(groupId, {
+    type: "text",
+    text: "Jobandwork! (" + Math.random() + ")"
+  });
+});
 
 app.listen(process.env.PORT || 5000, function() {
   console.log("LineBot is running.");
