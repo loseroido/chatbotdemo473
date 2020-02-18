a6f8d3e7817309c78e913bbe3d5d12b776ffe8b7
Sukhee <tab.suherdene@gmail.com>
2020-02-18 17:20:39 +0900

    Changed Sdk

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
-const linebot = require("linebot");
-const express = require("express");
+const express = require('express')
+const middleware = require('@line/bot-sdk').middleware
 
-const bot = linebot({
+const app = express()
+
+const config = {
   channelId: "1653837691",
   channelSecret: "de9040d7cbac1069321a5c80dfefc864",
   channelAccessToken:
     "Gv19n3lNQbMzkTolCguyopqUA/2ZQRT7jDBkgIUyAOXOZXe2tr76KfJxn3WIqu4DCRR9w2RkNs9sAfFXb43feMalCyqP0a2wHKN1N1Atls+kS6PJs8mOjkDUgNrgfFboX4ZSCuxxzFC7cmADJTHtLAdB04t89/1O/w1cDnyilFU="
-});
+}
 
-const app = express();
+app.post('/callback', middleware(config), (req, res) => {
+  req.body.events // webhook event objects
+  req.body.destination // user ID of the bot (optional)
+})
 
-const linebotParser = bot.parser();
-
-const logs = [];
-
-app.get("/", function(req, res) {
-  res.send(logs.toString());
-});
-
-app.post("/callback", linebotParser);
-
-bot.on("message", function(event) {
-  event
-    .reply(event.message.text)
-    .then(function(data) {
-      logs.push(data.toString());
-      console.log("Success", data);
-    })
-    .catch(function(error) {
-      console.log("Error", error);
-    });
-});
-
-app.listen(5000, function() {
-  console.log("LineBot is running.");
-  console.log("LineBot is running haha.");
-});
+app.listen(process.env.PORT || 8080)
