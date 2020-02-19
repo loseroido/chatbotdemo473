f75e95d20839c18da556d8a40afafcb3fdc59da0
Eba <50412313+Eba472@users.noreply.github.com>
2020-02-19 16:30:12 +0900

    x

--- template.js
+++ template.js
@@ -72,7 +72,7 @@ module.exports.test = {
 
   module.exports.imageMap= {
     type: 'imagemap',
-    baseUrl: 'https://chat-bot-demo-nodejs.herokuapp.com/images/imageMap',
+    baseUrl: 'https://chat-bot-demo-nodejs.herokuapp.com/images/imageMap1',
     altText: 'This is an imagemap',
     baseSize: {
       width: 1040,
