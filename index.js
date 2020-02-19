c3f2f442f52ab835995a0c7155eea053decdcf4e
Eba <50412313+Eba472@users.noreply.github.com>
2020-02-19 12:29:22 +0900

    s

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Ids = {
   ebaId:  "U382734647ff2b7f97b64132e0bab57f9"
 };
 
-client.linkRichMenuToUser(Ids.groupId, "<rich_menu_id>");
+client.linkRichMenuToUser(Ids.groupId, "richmenu-b937849f3570e3556c497dd20dcf1b23");
 
 app.post("/callback", line.middleware(config),async (req, res) => {
   console.log("New post");
