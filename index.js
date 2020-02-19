b80118e609ff03576ec47417dd9033769bd77535
Eba <50412313+Eba472@users.noreply.github.com>
2020-02-19 16:16:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const config = {
   channelAccessToken:
     "Gv19n3lNQbMzkTolCguyopqUA/2ZQRT7jDBkgIUyAOXOZXe2tr76KfJxn3WIqu4DCRR9w2RkNs9sAfFXb43feMalCyqP0a2wHKN1N1Atls+kS6PJs8mOjkDUgNrgfFboX4ZSCuxxzFC7cmADJTHtLAdB04t89/1O/w1cDnyilFU="
 };
-
 const client = new line.Client(config);
 const Ids = {
   groupId:"C3f4fe7ff4e6e5223810445b0146f2788",
