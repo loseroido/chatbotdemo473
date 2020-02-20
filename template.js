3676d6ef45bfd4a10862064a5a6a4ace0edb7514
Eba <50412313+Eba472@users.noreply.github.com>
2020-02-20 11:49:54 +0900

    k

--- template.js
+++ template.js
@@ -1,27 +1,4 @@
-module.exports.test = {
-    type: 'template',
-    altText: 'this is a buttons template',
-    template: {
-      type: 'buttons',
-      actions: [
-        {
-          type: 'message',
-          label: 'Action 1',
-          text: 'Action 1'
-        },
-        {
-          type: 'message',
-          label: 'Action 2',
-          text: 'Action 2'
-        }
-      ],
-      thumbnailImageUrl: 'https://chat-bot-demo-nodejs.herokuapp.com/images/start.png',
-      title: 'Title',
-      text: 'Text'
-    }
-  }
-  
-  module.exports.carousel = {
+module.exports.carousel = {
     type: 'template',
     altText: 'this is a carousel template',
     template: {
@@ -29,50 +6,14 @@ module.exports.test = {
       actions: [],
       columns: [
         {
-          thumbnailImageUrl: 'https://images.unsplash.com/photo-1528953030358-b0c7de371f1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
-          title: 'Bataa',
-          text: 'Student, No Japanese',
-          actions: [
-            {
-              type: 'postback',
-              label: 'Write text to him',
-              text: 'Write text to him',
-              data: 'Data 1'
-            },
-            {
-              type: 'message',
-              label: 'Action 2',
-              text: 'Action 2'
-            }
-          ]
-        },
-        {
-          thumbnailImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvkGRV7tBtNRhYZ_X4VXbX1tPgY3z4qSdwNBL3DeTNIMbHA00V',
-          title: 'Boldoo',
-          text: 'Full-time worker, Fluent',
-          actions: [
-            {
-              type: 'message',
-              label: 'Write text to him',
-              text: 'Action 1'
-            },
-            {
-              type: 'message',
-              label: 'Action 2',
-              text: 'Action 2'
-            }
-          ]
-        },
-        {
           thumbnailImageUrl: 'https://pluspng.com/img-png/png-worker-builder-png-image-392.png',
-          title: 'Turuu',
-          text: 'His hobby is football',
+          title: workers[1].name,
+          text: workers[1].phone_number,
           actions: [
             {
-              type: 'postback',
-              label: 'Reach line',
-              text: 'Action 1',
-              data: 'Turuu'
+              type: 'message',
+              label: 'Action 2',
+              text: 'Action 2'
             },
             {
               type: 'message',
@@ -83,63 +24,58 @@ module.exports.test = {
         },
         {
           thumbnailImageUrl: 'https://cdn2.vectorstock.com/i/1000x1000/66/36/nameplate-of-wood-with-word-menu-vector-3166636.jpg',
-          title: 'Menu',
-          text: 'Menu',
+          title: '',
+          text: '',
           actions: [
             {
               type: 'message',
               label: 'Menu',
               text: 'Menu'
             },
-            {
-              type: 'message',
-              label: 'Menu',
-              text: 'Menu'
-            }
           ]
         }
       ]
     }
   }
 
-  module.exports.imageMap= {
-    type: 'imagemap',
-    baseUrl: 'https://chat-bot-demo-nodejs.herokuapp.com/images/imageMap1',
-    altText: 'This is an imagemap',
-    baseSize: {
-      width: 1040,
-      height: 1040
-    },
-    actions: [
-      {
-        type: 'message',
-        text: 'I love Eba',
-        area: {
-          x: 0,
-          y: 0,
-          width: 520,
-          height: 1040
-        }
-      },
-      {
-        type: 'message',
-        text: 'Job&Work',
-        area: {
-          x: 520,
-          y: 0,
-          width: 520,
-          height: 1040
-        }
-      },
-      {
-        type: 'message',
-        text: 'Hello haha',
-        area: {
-          x: 520,
-          y: 520,
-          width: 1040,
-          height: 520
-        }
+module.exports.imageMap= {
+  type: 'imagemap',
+  baseUrl: 'https://chat-bot-demo-nodejs.herokuapp.com/images/imageMap1',
+  altText: 'This is an imagemap',
+  baseSize: {
+    width: 1040,
+    height: 1040
+  },
+  actions: [
+    {
+      type: 'message',
+      text: 'I love Eba',
+      area: {
+        x: 0,
+        y: 0,
+        width: 520,
+        height: 1040
       }
-    ]
-  }
+    },
+    {
+      type: 'message',
+      text: 'Job&Work',
+      area: {
+        x: 520,
+        y: 0,
+        width: 520,
+        height: 1040
+      }
+    },
+    {
+      type: 'message',
+      text: 'Hello haha',
+      area: {
+        x: 520,
+        y: 520,
+        width: 1040,
+        height: 520
+      }
+    }
+  ]
+}
