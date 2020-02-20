186f018753fab2ecbd4bcdf925d0ada38023421e
Eba <50412313+Eba472@users.noreply.github.com>
2020-02-20 16:01:06 +0900

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,12 +1,16 @@
-module.exports.statusOfWorkers= {
+const statusOfWorkers = {
     1: "Will come on time",
     2: "Will be late",
     0: "Won't work"
 }
+
+module.exports.statusOfWorkers= statusOfWorkers
+
 module.exports.workers = [{
     name: "Enkhbaatar",
     age: 20,
     status: 1,
+    state: statusOfWorkers[1],
     phone_number: "080-8122-0794",
     image: "https://chat-bot-demo-nodejs.herokuapp.com/images/workers/eba.jpg"
 },
@@ -14,6 +18,7 @@ module.exports.workers = [{
     name: "Bat",
     age: 25,
     status: 0,
+    state: statusOfWorkers[0],
     phone_number: "080-0000-0794",
     image: "https://chat-bot-demo-nodejs.herokuapp.com/images/workers/Bat.jpeg"
 },
@@ -21,6 +26,7 @@ module.exports.workers = [{
     name: "BatBold",
     age: 28,
     status: 1,
+    state: statusOfWorkers[1],
     phone_number: "0120-3400-0544",
     image: "https://chat-bot-demo-nodejs.herokuapp.com/images/workers/BatBold.jpeg"
 },
@@ -28,6 +34,7 @@ module.exports.workers = [{
     name: "Dorj",
     age: 26,
     status: 1,
+    state: statusOfWorkers[1],
     phone_number: "0120-3400-0544",
     image: "https://chat-bot-demo-nodejs.herokuapp.com/images/workers/Dorj.jpeg"
 }]
