8d7e83e1cfcc83528e9005294793d3116e560344
Eba <50412313+Eba472@users.noreply.github.com>
2020-02-20 12:32:47 +0900

    Update template.js

--- template.js
+++ template.js
@@ -1,46 +1,84 @@
 const {workers}=require("./workers");
 module.exports.carousel = {
-    type: 'template',
-    altText: 'this is a carousel template',
-    template: {
-      type: 'carousel',
-      actions: [],
-      columns: [
-        {
-          thumbnailImageUrl: 'https://pluspng.com/img-png/png-worker-builder-png-image-392.png',
-          // title: workers[1].name,
-          // text: workers[1].phone_number,
-          title: 'Eba',
-          text: 'cdbj',
-          actions: [
-            {
-              type: 'message',
-              label: 'Action 2',
-              text: 'Action 2'
-            },
-            {
-              type: 'message',
-              label: 'Action 2',
-              text: 'Action 2'
-            }
-          ]
-        },
-        {
-          thumbnailImageUrl: 'https://cdn2.vectorstock.com/i/1000x1000/66/36/nameplate-of-wood-with-word-menu-vector-3166636.jpg',
-          title: 'k',
-          text: 'ls',
-          actions: [
-            {
-              type: 'message',
-              label: 'Menu',
-              text: 'Menu'
-            },
-          ]
-        }
-      ]
-    }
+  type: 'template',
+  altText: 'this is a carousel template',
+  template: {
+    type: 'carousel',
+    actions: [],
+    columns: [
+      {
+        thumbnailImageUrl: 'https://images.unsplash.com/photo-1528953030358-b0c7de371f1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+        title: 'Bataa',
+        text: 'Student, No Japanese',
+        actions: [
+          {
+            type: 'postback',
+            label: 'Write text to him',
+            text: 'Write text to him',
+            data: 'Data 1'
+          },
+          {
+            type: 'message',
+            label: 'Action 2',
+            text: 'Action 2'
+          }
+        ]
+      },
+      {
+        thumbnailImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvkGRV7tBtNRhYZ_X4VXbX1tPgY3z4qSdwNBL3DeTNIMbHA00V',
+        title: 'Boldoo',
+        text: 'Full-time worker, Fluent',
+        actions: [
+          {
+            type: 'message',
+            label: 'Write text to him',
+            text: 'Action 1'
+          },
+          {
+            type: 'message',
+            label: 'Action 2',
+            text: 'Action 2'
+          }
+        ]
+      },
+      {
+        thumbnailImageUrl: 'https://pluspng.com/img-png/png-worker-builder-png-image-392.png',
+        title: 'Turuu',
+        text: 'His hobby is football',
+        actions: [
+          {
+            type: 'postback',
+            label: 'Reach line',
+            text: 'Action 1',
+            data: 'Turuu'
+          },
+          {
+            type: 'message',
+            label: 'Action 2',
+            text: 'Action 2'
+          }
+        ]
+      },
+      {
+        thumbnailImageUrl: 'https://cdn2.vectorstock.com/i/1000x1000/66/36/nameplate-of-wood-with-word-menu-vector-3166636.jpg',
+        title: 'Menu',
+        text: 'Menu',
+        actions: [
+          {
+            type: 'message',
+            label: 'Menu',
+            text: 'Menu'
+          },
+          {
+            type: 'message',
+            label: 'Menu',
+            text: 'Menu'
+          }
+        ]
+      }
+    ]
   }
-
+}
 module.exports.imageMap= {
   type: 'imagemap',
   baseUrl: 'https://chat-bot-demo-nodejs.herokuapp.com/images/imageMap1',
